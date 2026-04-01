a78b299cb4008aa25589b8d332fe8813103652ba
shiphrahjasmine27 <shiphrahjasmine.27csb@licet.ac.in>
2026-04-01 08:42:43 +0530

    2nd question

---
+++ src/test/java/com/example/examdemo/ExamdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
